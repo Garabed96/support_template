fcc33997ba4eac5638c15afe797a438bf3f9dcdc
Garabed96 <garonazarian09@gmail.com>
2023-06-02 23:45:15 +0700

    api endpoint /discord

--- app/discord/route.ts
+++ app/discord/route.ts
@@ -5,9 +5,7 @@ import { NextResponse } from "next/server";
 export async function GET(request) {
   const supabase = createRouteHandlerClient({ cookies });
 
-  const { data } = await supabase.auth.signInWithOAuth({
-    provider: "discord",
-  });
+  const { data } = await supabase.auth.getSession();
   // URL to redirect to after sign in process completes
   return NextResponse.json(data);
 }
