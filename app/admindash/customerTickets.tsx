49d445f8d5491cd45f32881bfae3710120e88d98
Garabed96 <garonazarian09@gmail.com>
2023-06-04 00:24:20 +0700

    /admindash table stying 2 (Pagination working)

--- app/admindash/customerTickets.tsx
+++ app/admindash/customerTickets.tsx
@@ -1,42 +1,117 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { createClient } from "@/utils/supabase-browser";
-import { Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  Button,
+} from "@chakra-ui/react";
 
-export default async function CustomerTickets() {
+const PAGE_SIZE = 5; // Number of items per page
+
+export const getPagination = (page, size) => {
+  const limit = size ? +size : PAGE_SIZE;
+  const from = page ? (page - 1) * limit : 0;
+  const to = page ? from + limit : limit;
+
+  return { from, to };
+};
+
+export default function CustomerTickets() {
   const supabase = createClient();
+  const [selectedPage, setSelectedPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [data, setData] = useState([]);
 
-  const { data } = await supabase.from("support_ticket").select();
+  const fetchData = async (page) => {
+    const { from, to } = getPagination(page);
+    const { data: tickets } = await supabase
+      .from("support_ticket")
+      .select()
+      .range(from, to);
+
+    setData(tickets);
+  };
+
+  const fetchTotalCount = async () => {
+    const { from, to } = getPagination(1);
+    const { data, count } = await supabase
+      .from("support_ticket")
+      .select("*", { count: "exact" })
+      .order("id", { ascending: true })
+      .range(from, to);
+
+    setTotalPages(Math.ceil(count / PAGE_SIZE));
+  };
+
+  const handlePageChange = (page) => {
+    setSelectedPage(page);
+  };
+
+  useEffect(() => {
+    fetchData(selectedPage);
+  }, [selectedPage]);
+
+  useEffect(() => {
+    fetchTotalCount();
+  }, []);
 
   return (
-    <Table variant="striped" colorScheme="gray">
-      <Thead>
-        <Tr>
-          <Th>ID</Th>
-          <Th>Created At</Th>
-          <Th>Ref ID</Th>
-          <Th>BTC Txn Hash</Th>
-          <Th>Ticket Type</Th>
-          <Th>Message</Th>
-          <Th>Updated At</Th>
-          <Th>Discord ID</Th>
-          <Th>Is Complete</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {data.map((ticket) => (
-          <Tr key={ticket.id}>
-            <Td>{ticket.id}</Td>
-            <Td>{ticket.created_at}</Td>
-            <Td>{ticket.ref_id}</Td>
-            <Td>{ticket.btc_txn_hash}</Td>
-            <Td>{ticket.ticket_type}</Td>
-            <Td>{ticket.message}</Td>
-            <Td>{ticket.updated_at}</Td>
-            <Td>{ticket.discord_id}</Td>
-            <Td>{ticket.is_complete.toString()}</Td>
-          </Tr>
-        ))}
-      </Tbody>
-    </Table>
+    <>
+      <TableContainer>
+        <Table
+          variant="simple"
+          p="5"
+          size="lg"
+          variant="striped"
+          colorScheme="gray"
+        >
+          <Thead>
+            <Tr>
+              <Th>ID</Th>
+              <Th>Created At</Th>
+              <Th>Ref ID</Th>
+              <Th>BTC Txn Hash</Th>
+              <Th>Ticket Type</Th>
+              <Th>Message</Th>
+              <Th>Updated At</Th>
+              <Th>Discord ID</Th>
+              <Th>Is Complete</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {data.map((ticket) => (
+              <Tr key={ticket.id}>
+                <Td>{ticket.id}</Td>
+                <Td>{ticket.created_at}</Td>
+                <Td>{ticket.ref_id}</Td>
+                <Td>{ticket.btc_txn_hash}</Td>
+                <Td>{ticket.ticket_type}</Td>
+                <Td>{ticket.message}</Td>
+                <Td>{ticket.updated_at}</Td>
+                <Td>{ticket.discord_id}</Td>
+                <Td>{ticket.is_complete.toString()}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+      <Button
+        onClick={() => handlePageChange(selectedPage - 1)}
+        disabled={selectedPage === 1}
+      >
+        Previous
+      </Button>
+      <Button
+        onClick={() => handlePageChange(selectedPage + 1)}
+        disabled={selectedPage === totalPages}
+      >
+        Next
+      </Button>
+    </>
   );
 }
