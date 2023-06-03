2d9a24187fa68ae3a2a94be2c03d7f882b1a210c
Garabed96 <garonazarian09@gmail.com>
2023-06-03 22:17:24 +0700

    can't submit TicketForm.tsx without login

--- app/ticket/TicketForm.tsx
+++ app/ticket/TicketForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Flex,
   Heading,
@@ -13,6 +13,7 @@ import {
   Input,
   Textarea,
 } from "@chakra-ui/react";
+import { checkSession } from "@/components/userActions/checkSession";
 const TicketForm = () => {
   const [formData, setFormData] = useState({
     discordAccount: "",
@@ -28,6 +29,23 @@ const TicketForm = () => {
       [name]: value,
     }));
   };
+  const [user, setUser] = useState<Object | null>(null);
+  useEffect(() => {
+    const getSession = async () => {
+      try {
+        const session = await checkSession();
+        if (!session) {
+          console.log("NO SESSION, NULL");
+        } else if (session) {
+          console.log("SESSION: ", session);
+          setUser(session);
+        }
+      } catch (e) {
+        console.log("ERRORLOG");
+      }
+    };
+    getSession();
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -46,109 +64,115 @@ const TicketForm = () => {
   const isError = input === "";
 
   return (
-    <Flex
-      direction="column"
-      align="center"
-      justify="center"
-      minHeight="100vh"
-      pb="4rem"
-    >
-      <Heading as="h2" size="lg" marginBottom="1rem">
-        Create Support Ticket
-      </Heading>
-      <Text fontSize="md" color="gray.300" marginBottom="2rem">
-        <Text as="span" color="green.400">
-          Please
-        </Text>{" "}
-        fill out the form below to create a support ticket.{" "}
-        <Text as="span" color="green.400">
-          Our
-        </Text>{" "}
-        team will assist{" "}
-        <Text as="span" color="green.400">
-          you
-        </Text>{" "}
-        as soon as{" "}
-        <Text as="span" color="green.400">
-          possible.
-        </Text>{" "}
-      </Text>
-      <Box width="400px" boxShadow="dark-lg" p={4} rounded="md">
-        <form onSubmit={handleSubmit}>
-          <FormControl id="discordAccount" my={4} isRequired>
-            <FormLabel>Discord Account</FormLabel>
-            <Input
-              type="text"
-              name="discordAccount"
-              value={formData.discordAccount}
-              onChange={handleChange}
-              placeholder="Enter your Discord account"
-              required
-            />
-            {!isError ? (
-              <FormHelperText>
-                Enter the email you'd like to receive the newsletter on.
-              </FormHelperText>
-            ) : (
-              <FormErrorMessage>Email is required.</FormErrorMessage>
-            )}
-          </FormControl>
+    <div>
+      {user ? (
+        <Flex
+          direction="column"
+          align="center"
+          justify="center"
+          minHeight="100vh"
+          pb="4rem"
+        >
+          <Heading as="h2" size="lg" marginBottom="1rem">
+            Create Support Ticket
+          </Heading>
+          <Text fontSize="md" color="gray.300" marginBottom="2rem">
+            <Text as="span" color="green.400">
+              Please
+            </Text>{" "}
+            fill out the form below to create a support ticket.{" "}
+            <Text as="span" color="green.400">
+              Our
+            </Text>{" "}
+            team will assist{" "}
+            <Text as="span" color="green.400">
+              you
+            </Text>{" "}
+            as soon as{" "}
+            <Text as="span" color="green.400">
+              possible.
+            </Text>{" "}
+          </Text>
+          <Box width="400px" boxShadow="dark-lg" p={4} rounded="md">
+            <form onSubmit={handleSubmit}>
+              <FormControl id="discordAccount" my={4} isRequired>
+                <FormLabel>Discord Account</FormLabel>
+                <Input
+                  type="text"
+                  name="discordAccount"
+                  value={formData.discordAccount}
+                  readOnly // Set the readOnly attribute to prevent user input
+                  placeholder="Enter your Discord account"
+                  required
+                />
+                {!isError ? (
+                  <FormHelperText>
+                    Enter the email you'd like to receive the newsletter on.
+                  </FormHelperText>
+                ) : (
+                  <FormErrorMessage>Email is required.</FormErrorMessage>
+                )}
+              </FormControl>
 
-          <FormControl id="refId" my={4} isRequired>
-            <FormLabel>Ref ID</FormLabel>
-            <Input
-              type="text"
-              name="refId"
-              value={formData.refId}
-              onChange={handleChange}
-              placeholder="Enter your Ref ID"
-              required
-            />
-          </FormControl>
+              <FormControl id="refId" my={4} isRequired>
+                <FormLabel>Ref ID</FormLabel>
+                <Input
+                  type="text"
+                  name="refId"
+                  value={formData.refId}
+                  onChange={handleChange}
+                  placeholder="Enter your Ref ID"
+                  required
+                />
+              </FormControl>
 
-          <FormControl id="hashcode" my={4} isRequired>
-            <FormLabel>Eth or BTC Hash Code</FormLabel>
-            <Input
-              type="text"
-              name="hashcode"
-              value={formData.hashcode}
-              onChange={handleChange}
-              placeholder="Enter your Eth or BTC Hash Code"
-              required
-            />
-          </FormControl>
-          <FormControl isRequired>
-            <FormLabel>Ticket Type</FormLabel>
-            <Select placeholder="Select an option">
-              <option value="status-update">Status Update</option>
-              <option value="status-update">Billing Inquiry</option>
-              <option value="status-update">Technical Issue</option>
-              <option value="status-update">Feedback</option>
-              <option value="refund">Refund</option>
-              <option value="status-update">Other</option>
-            </Select>
-          </FormControl>
-          <Textarea
-            placeholder="Write a personalized message to the support team here"
-            size="lg"
-            borderRadius="md"
-            focusBorderColor="green.400"
-            _placeholder={{ color: "gray.400" }}
-            mt={4}
-          />
-          <Flex justify="flex-end">
-            <Button
-              type="submit"
-              colorScheme="green"
-              _hover={{ bg: "green.400" }}
-              mt={8}
-            >
-              Submit
-            </Button>
-          </Flex>
-        </form>
-      </Box>
-    </Flex>
+              <FormControl id="hashcode" my={4} isRequired>
+                <FormLabel>Eth or BTC Hash Code</FormLabel>
+                <Input
+                  type="text"
+                  name="hashcode"
+                  value={formData.hashcode}
+                  onChange={handleChange}
+                  placeholder="Enter your Eth or BTC Hash Code"
+                  required
+                />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>Ticket Type</FormLabel>
+                <Select placeholder="Select an option">
+                  <option value="status-update">Status Update</option>
+                  <option value="status-update">Billing Inquiry</option>
+                  <option value="status-update">Technical Issue</option>
+                  <option value="status-update">Feedback</option>
+                  <option value="refund">Refund</option>
+                  <option value="status-update">Other</option>
+                </Select>
+              </FormControl>
+              <Textarea
+                placeholder="Write a personalized message to the support team here"
+                size="lg"
+                borderRadius="md"
+                focusBorderColor="green.400"
+                _placeholder={{ color: "gray.400" }}
+                mt={4}
+              />
+              <Flex justify="flex-end">
+                <Button
+                  type="submit"
+                  colorScheme="green"
+                  _hover={{ bg: "green.400" }}
+                  mt={8}
+                >
+                  Submit
+                </Button>
+              </Flex>
+            </form>
+          </Box>
+        </Flex>
+      ) : (
+        <>please log in</>
+      )}
+    </div>
   );
 };
 
