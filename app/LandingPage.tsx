8b1a552c86663e7136386e10d0a7a6db199ddf37
Garabed96 <garonazarian09@gmail.com>
2023-06-03 15:12:54 +0700

    conditional, if user logged in, reroute to /ticket

--- app/LandingPage.tsx
+++ app/LandingPage.tsx
@@ -17,10 +17,9 @@ import { useAuth } from "@/components/providers/supabase-auth-provider";
 import { checkSession } from "@/components/userActions/checkSession";
 const LandingPage = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const { signInWithDiscord, sessionUser } = useAuth();
-  console.log("THIS IS SESSION USER", sessionUser);
-  // Q: This is another way of importing SupaBase, is it better than just doing the standard createClient in utils/supabase.ts ?
-  // const supabase = useSupabaseClient();
+  const { signInWithDiscord } = useAuth();
+
+  const [user, setUser] = useState<Object | null>(null);
   useEffect(() => {
     const getSession = async () => {
       try {
@@ -29,6 +28,7 @@ const LandingPage = () => {
           console.log("NO SESSION, NULL");
         } else if (session) {
           console.log("SESSION: ", session);
+          setUser(session);
         }
       } catch (e) {
         console.log("ERRORLOG");
@@ -64,65 +64,70 @@ const LandingPage = () => {
         a ticket to get personalized support.
       </Text>
       <Box w="50%" rounded="md" variant="outline" textAlign="center">
-        {/*<Link href="/ticket">*/}
-        {/*  <Button*/}
-        {/*    w="50%"*/}
-        {/*    _hover={{ bg: "green.400" }}*/}
-        {/*    rounded="sm"*/}
-        {/*    variant="outline"*/}
-        {/*    rightIcon={<EditIcon />}*/}
-        {/*  >*/}
-        {/*    Create Ticket*/}
-        {/*  </Button>*/}
-        {/*</Link>        */}
-        <Button
-          w="50%"
-          _hover={{ bg: "green.400" }}
-          rounded="sm"
-          variant="outline"
-          rightIcon={<EditIcon />}
-          onClick={onOpen}
-        >
-          Create Ticket
-        </Button>
-        <Modal
-          isCentered
-          onClose={onClose}
-          isOpen={isOpen}
-          motionPreset="slideInBottom"
-        >
-          <ModalOverlay />
-          <ModalContent>
-            <ModalCloseButton />
-            <ModalHeader mt="8">
-              <div align="center">
-                <Text as="span" color="green.400">
-                  Authenticate
-                </Text>{" "}
-                through{" "}
-                <Text as="span" color="green.400">
-                  Discord
-                </Text>{" "}
-                to create a{" "}
-                <Text as="span" color="green.400">
-                  Ticket
-                </Text>
-              </div>
-            </ModalHeader>
-            <ModalFooter mx="8" mb="8">
-              <Button
-                fontSize="sm"
-                fontWeight="normal"
-                rightIcon={<ArrowForwardIcon />}
-                variant="outline"
-                onClick={signInWithDiscord}
-              >
-                Login with Discord
-              </Button>
-              {/*<button>Login</button>*/}
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
+        {user ? (
+          <Link href="/ticket">
+            <Button
+              w="50%"
+              _hover={{ bg: "green.400" }}
+              rounded="sm"
+              variant="outline"
+              rightIcon={<EditIcon />}
+            >
+              Create Ticket
+            </Button>
+          </Link>
+        ) : (
+          <>
+            <Button
+              w="50%"
+              _hover={{ bg: "green.400" }}
+              rounded="sm"
+              variant="outline"
+              rightIcon={<EditIcon />}
+              onClick={onOpen}
+            >
+              Create Ticket
+            </Button>
+            <Modal
+              isCentered
+              onClose={onClose}
+              isOpen={isOpen}
+              motionPreset="slideInBottom"
+            >
+              <ModalOverlay />
+              <ModalContent>
+                <ModalCloseButton />
+                <ModalHeader mt="8">
+                  <div align="center">
+                    <Text as="span" color="green.400">
+                      Authenticate
+                    </Text>{" "}
+                    through{" "}
+                    <Text as="span" color="green.400">
+                      Discord
+                    </Text>{" "}
+                    to create a{" "}
+                    <Text as="span" color="green.400">
+                      Ticket
+                    </Text>
+                  </div>
+                </ModalHeader>
+                <ModalFooter mx="8" mb="8">
+                  <Button
+                    fontSize="sm"
+                    fontWeight="normal"
+                    rightIcon={<ArrowForwardIcon />}
+                    variant="outline"
+                    onClick={signInWithDiscord}
+                  >
+                    Login with Discord
+                  </Button>
+                  {/*<button>Login</button>*/}
+                </ModalFooter>
+              </ModalContent>
+            </Modal>
+          </>
+        )}
       </Box>
     </Flex>
   );
