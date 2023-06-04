59a30ef1b4e7748e0aa9949985967352026148b3
Garabed96 <garonazarian09@gmail.com>
2023-06-04 23:01:06 +0700

    TODO: White flash docs

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import Layout from "../components/layout";
 import React from "react";
 import LandingPage from "./LandingPage";
 
+// TODO: Fix White flash
+// https://stackoverflow.com/questions/70297964/next-js-how-to-prevent-flash-of-the-unauthorized-route-page-prior-to-redirect-w
 export default function Home() {
   return (
     <main>
