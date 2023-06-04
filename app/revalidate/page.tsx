c2bc3fb2905078d13e6885027c9baa958253dbeb
Garabed96 <garonazarian09@gmail.com>
2023-06-04 23:49:59 +0700

    cleanup

--- app/revalidate/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { supportClient } from "@/utils/supabase-browser";
-
-export default async function Posts() {
-  const supabase = supportClient();
-
-  const { data } = await supabase.from("support_ticket").select();
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
-}
